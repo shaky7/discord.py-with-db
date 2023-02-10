3f0001e4cdafe0a7b014f384ad9d3cce4f9e65cb
shaky <119557802+shaky7@users.noreply.github.com>
2023-02-09 23:37:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ intents = discord.Intents.default()
 intents.members = True
 prefix = "!"
 bot = commands.Bot(command_prefix=prefix, help_command=None, intents=intents)
-token = "MTA3MzQyMjc1OTE4NDU2ODQzMQ.GJkJcR.mr9PbVHgUkLzqr2Y5HS0qQqOec-_YC1I8GVhqI"
+token = ""
 
 
 @bot.event
